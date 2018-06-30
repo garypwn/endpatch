8f263979c6c586dc6795f8632a5823467c1262ae
garypwn <garypwn@users.noreply.github.com>
2018-06-30 15:57:43 -0700

    Prevent dragon egg teleportation and physics

--- src/main/java/net/comecraft/endpatch/DragonEggPatch.java
+++ src/main/java/net/comecraft/endpatch/DragonEggPatch.java
@@ -4,11 +4,35 @@ import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockFromToEvent;
+import org.bukkit.event.block.BlockPhysicsEvent;
 
+/**
+ * DragonEggPatch attempts to address broken behaviour with dragon eggs. It
+ * stops dragon egg teleportation and disables dragon egg physics.
+ */
 public class DragonEggPatch implements Listener {
 
+	/**
+	 * Stop dragon egg teleportation.
+	 * 
+	 * @param event
+	 *            The event fired by the dragon egg trying to teleport.
+	 */
 	@EventHandler
 	public void onDragonEggTp(BlockFromToEvent event) {
+		if (event.getBlock().getType().equals(Material.DRAGON_EGG)) {
+			event.setCancelled(true);
+		}
+	}
+
+	/**
+	 * Stop dragon egg physics if you break the block below the dragon egg.
+	 * 
+	 * @param event
+	 *            The event fired when breaking a block below a dragon egg.
+	 */
+	@EventHandler
+	public void onDragonEggPhysics(BlockPhysicsEvent event) {
 		if (event.getBlock().getType().equals(Material.DRAGON_EGG)) {
 			event.setCancelled(true);
 		}
