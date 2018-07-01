2bf342ede226c8a9db28e39c1aa53a8afb6d51c6
garypwn <garypwn@users.noreply.github.com>
2018-06-30 17:09:19 -0700

    Add documentation to EndGatewayPatch

--- src/main/java/net/comecraft/endpatch/EndGatewayPatch.java
+++ src/main/java/net/comecraft/endpatch/EndGatewayPatch.java
@@ -10,20 +10,31 @@ import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
 import com.onarandombox.MultiverseCore.api.Core;
 import com.onarandombox.MultiverseCore.api.MVDestination;
 
+/**
+ * EndGatewayPatch provides a workaround for a bug in minecraft where return
+ * gateways from the outer islands send players to unsafe locations.
+ */
 public class EndGatewayPatch implements Listener {
 
+	/**
+	 * Intercepts teleport events from outer island return gateways.
+	 * 
+	 * @param event
+	 *            The event fired by a player teleporting through a return gateway
+	 *            from the end outer islands.
+	 */
 	@EventHandler
 	public void onPlayerTpEvent(PlayerTeleportEvent event) {
 		if (event.getCause().equals(TeleportCause.END_GATEWAY)) {
-			
+
 			Player player = event.getPlayer();
-			
+
 			// Do nothing if the player is on the centre island
 			// TODO put distance in config
 			if (player.getLocation().distance(player.getWorld().getSpawnLocation()) < 800) return;
-			
+
 			event.setCancelled(true);
-			
+
 			// Teleport the player
 			// TODO put destination in config
 			Core multiverse = (Core) Bukkit.getPluginManager().getPlugin("Multiverse-Core");
