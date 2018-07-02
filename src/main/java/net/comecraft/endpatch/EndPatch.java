bc682980c58351d9d9cb4c4f118647c123a1938f
garypwn <garypwn@users.noreply.github.com>
2018-07-01 19:39:44 -0700

    Fix formatting in EndPatch

--- src/main/java/net/comecraft/endpatch/EndPatch.java
+++ src/main/java/net/comecraft/endpatch/EndPatch.java
@@ -14,30 +14,30 @@ public class EndPatch extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
-		
+
 		setDefaults();
-		
+
 		// Register event handlers
 		getServer().getPluginManager().registerEvents(new DragonEggPatch(), this);
 		getServer().getPluginManager().registerEvents(new EndGatewayPatch(), this);
 	}
-	
+
 	// Gets default config values from a file and adds them to the fileconfig
-		private void setDefaults() {
-			
-			// Get default config from resources folder
-			InputStream defaults = getResource("configDefault.yml");
-			Reader defaultReader = new InputStreamReader(defaults);
-			Configuration defaultConfig = YamlConfiguration.loadConfiguration(defaultReader);
-			
-			// Set default config
-			this.getConfig().setDefaults(defaultConfig);
-			
-			try {
-				defaultReader.close();
-			} catch (IOException e) {
-				// Log the error
-				Bukkit.getLogger().warning("[endpatch] error reading default configuration.");
-			}
+	private void setDefaults() {
+
+		// Get default config from resources folder
+		InputStream defaults = getResource("configDefault.yml");
+		Reader defaultReader = new InputStreamReader(defaults);
+		Configuration defaultConfig = YamlConfiguration.loadConfiguration(defaultReader);
+
+		// Set default config
+		this.getConfig().setDefaults(defaultConfig);
+
+		try {
+			defaultReader.close();
+		} catch (IOException e) {
+			// Log the error
+			Bukkit.getLogger().warning("[endpatch] error reading default configuration.");
 		}
+	}
 }
